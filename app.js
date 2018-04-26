6dfe0cc791a9de654e330037531c56418b1512f1
redchenjs <redchenjs@live.com>
2018-04-26 16:35:10 +0800

    app: change default server url & remove unused functions

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-//app.js
-const util = require('./utils/util.js')
+// app.js
+const util = require('./utils/util.js');
 
 App({
     onLaunch: function () {
@@ -8,42 +8,29 @@ App({
         logs.unshift(Date.now());
         wx.setStorageSync('logs', logs);
 
-        wx.getHCEState({
-            success: function (res) {
-                getApp().globalData.nfcAvaliable = true;
-            },
-            fail: function (res) {
-                getApp().globalData.nfcAvaliable = false;
-            },
-            complete: function (res) {
-                console.log("nfcAvaliable: " + getApp().globalData.nfcAvaliable);
-            }
-        });
-
         wx.login({
             success: function (res) {
-                console.log("userCode: " + res.code);
                 getApp().globalData.userCode = res.code;
             }
-        })
+        });
 
         // 获取用户信息
         wx.getSetting({
             success: res => {
                 if (res.authSetting['scope.userInfo']) {
-                // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-                wx.getUserInfo({
-                    success: res => {
-                    // 可以将 res 发送给后台解码出 unionId
-                    this.globalData.userInfo = res.userInfo
+                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                    wx.getUserInfo({
+                        success: res => {
+                            // 可以将 res 发送给后台解码出 unionId
+                            this.globalData.userInfo = res.userInfo;
 
-                    // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-                    // 所以此处加入 callback 以防止这种情况
-                    if (this.userInfoReadyCallback) {
-                        this.userInfoReadyCallback(res)
-                    }
-                    }
-                })
+                            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                            // 所以此处加入 callback 以防止这种情况
+                            if (this.userInfoReadyCallback) {
+                                this.userInfoReadyCallback(res);
+                            }
+                        }
+                    });
                 }
             }
         });
@@ -52,7 +39,6 @@ App({
     globalData: {
         userInfo: null,
         userCode: null,
-        serverUrl: 'https://redchenjs.vicp.net/nas/',
-        nfcAvaliable: null
+        serverUrl: 'https://server.zyiot.top/nas/'
     }
 });
