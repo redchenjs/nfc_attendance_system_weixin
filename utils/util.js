6da486d67b606dd02681761bd428cd9c6624e178
Jack Chen <redchenjs@live.com>
2020-01-07 22:19:16 +0800

    utils: update util.js

--- utils/util.js
+++ utils/util.js
@@ -1,4 +1,4 @@
-// utils.js
+// util.js
 const formatTime = date => {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
@@ -15,8 +15,8 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n;
 }
 
-function buf2hex(buffer) { // buffer is an ArrayBuffer
-  return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
+function ab2hex(buf) {
+  return Array.prototype.map.call(new Uint8Array(buf), x => ('00' + x.toString(16)).slice(-2)).join('');
 }
 
 function ab2str(buf) {
@@ -24,7 +24,7 @@ function ab2str(buf) {
 }
 
 function str2ab(str) {
-  var buf = new ArrayBuffer(str.length); // 2 bytes for each char
+  var buf = new ArrayBuffer(str.length);
   var bufView = new Uint8Array(buf);
   for (var i = 0, strLen = str.length; i < strLen; i++) {
     bufView[i] = str.charCodeAt(i);
@@ -34,7 +34,7 @@ function str2ab(str) {
 
 module.exports = {
   formatTime: formatTime,
-  buf2hex: buf2hex,
+  ab2hex: ab2hex,
   ab2str: ab2str,
-  str2ab: str2ab
+  str2ab: str2ab,
 }
