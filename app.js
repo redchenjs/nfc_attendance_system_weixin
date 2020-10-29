470e1a52a8247b4ba191dcea70811a468bf596d6
Jack Chen <redchenjs@live.com>
2020-10-29 22:53:37 +0800

    app: add update manager

--- app.js
+++ app.js
@@ -10,10 +10,23 @@ export const reqCode = {
 
 App({
   onLaunch() {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || [];
-    logs.unshift(Date.now());
-    wx.setStorageSync('logs', logs);
+    const updateManager = wx.getUpdateManager();
+
+    updateManager.onCheckForUpdate(function (res) {
+      console.log('hasUpdate:', res.hasUpdate);
+    });
+
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success(res) {
+          if (res.confirm) {
+            updateManager.applyUpdate();
+          }
+        }
+      });
+    });
 
     wx.login({
       success(res) {
